8f13162ddcdf380e2cb95fa1f8d082c6e05bc7a1
Luís Peixoto <lmpeixoto@gmail.com>
2020-07-17 12:14:18 +0100

    added freguesias json

--- sandbox_instant_data.js
+++ sandbox_instant_data.js
@@ -1,33 +1,33 @@
-const axios = require('axios');
+const axios = require("axios");
 
 let occurrences = [];
 
 const BASE_URL =
-    'http://www.prociv.pt/_vti_bin/ARM.ANPC.UI/ANPC_SituacaoOperacional.svc/';
+  "http://www.prociv.pt/_vti_bin/ARM.ANPC.UI/ANPC_SituacaoOperacional.svc/";
 
 function getHistoryOccurrencesByLocation() {
-    axios
-        .post(BASE_URL + 'GetHistoryOccurrencesByLocation', {
-            distritoID: null,
-            concelhoID: null,
-            freguesiaID: null,
-            pageSize: 10000,
-            pageIndex: 1,
-            forToday: false,
-            natureza: '0'
-        })
-        .then(
-            (response) => {
-                occurrences.push(
-                    response.data.GetHistoryOccurrencesByLocationResult
-                        .ArrayInfo[0].Data[0]
-                );
-                console.log(occurrences);
-            },
-            (error) => {
-                console.log(error);
-            }
+  axios
+    .post(BASE_URL + "GetHistoryOccurrencesByLocation", {
+      distritoID: null,
+      concelhoID: null,
+      freguesiaID: null,
+      pageSize: 10000,
+      pageIndex: 1,
+      forToday: false,
+      natureza: "0",
+    })
+    .then(
+      (response) => {
+        occurrences.push(
+          response.data.GetHistoryOccurrencesByLocationResult.ArrayInfo[0]
+            .Data[0]
         );
+        console.log(occurrences);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
 }
 
 getHistoryOccurrencesByLocation();
